b18f84f641f6d9addec23869996f5dd6ec4c45a2
Jason A. Martin <jam@serabyte.com>
2019-06-01 14:04:59 -0400

    adding files before move.

--- loop.sh
+++ loop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for i in {1..45}
+for i in {1..420}
 do
    echo "Running Skeets: $i"
    #ruby Skeets.rb downloadimages
-   ruby Skeets.rb --command updateimages --quantity 10
-   sleep $((2 + RANDOM % 10))
+   ruby Skeets.rb --command downloadimages --quantity 20
+   sleep $((2 + RANDOM % 30))
 done
